2357709adfe23f84e5388e3781b9ee140a00f0d2
JunaidKp <junaid.kp@travvise.com>
2022-08-09 09:11:35 -0700

    new changes

--- src/app/services/flight-search-api.service.ts
+++ src/app/services/flight-search-api.service.ts
@@ -10,7 +10,8 @@ import { FlightOrdersRequest } from '../Models/FlightOrdersRequest';
 })
 export class FlightSearchApiService {
 
-  readonly flightsearchurl = "https://localhost:44318/";
+  // readonly flightsearchurl = "http://demo.konga.com:3000/";
+  readonly flightsearchurl = "https://localhost:44317/";
 
   constructor(private http:HttpClient) { }
 
